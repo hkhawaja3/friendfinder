58af70137145b6097f055590adb7b91526eafb94
Hamza Khawaja <hkhawaja3@gmail.com>
2017-10-11 17:28:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
-
-app.set('port', (process.env.PORT || 3000));
-
-var friends = require('./app/data/friends.js');
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(bodyParser.json({
-    type: "application/vnd.api+json"
-}));
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-app.use(express.static('public'));
-
-app.use(require('./app/routing/apiRoutes.js'));
-app.use(require('./app/routing/htmlRoutes.js'));
-
-app.listen(app.get('port'), function(){
-	console.log('Server running on port: ' + app.get('port'));
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
