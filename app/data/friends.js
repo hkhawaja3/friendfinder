04e027e03f19f10549978c003c9b3415410a998c
Hamza Khawaja <hkhawaja3@gmail.com>
2017-10-14 20:39:22 -0500

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,11 @@
 var friends = [
 	{
-		name: "Billy",
-		photo: "http://www.ideaeconomy.net/wp-content/uploads/2010/11/businessbilly.png",
+		name: "Hamza",
+		photo: "https://scontent-dft4-1.xx.fbcdn.net/v/t31.0-8/18620894_10154483017732761_2707541377990554654_o.jpg?oh=6a88e89800ae4fe2cbd9349b97d9af7c&oe=5A7E5508",
 		scores:[2, 3, 1, 4, 2, 3, 2, 2, 4, 1]
 	},{
-		name: "Johnny",
-		photo: "https://vignette.wikia.nocookie.net/cartoons/images/a/a7/JohnnyBravo3.gif/revision/latest?cb=20131103074449",
+		name: "Emmanuel",
+		photo: "https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/15319036_10207332253176523_57647237629838845_n.jpg?oh=10746fc365176eecea7619e089e3970e&oe=5A878C74",
 		scores: [5, 3, 4, 3, 1, 4, 2, 1, 3, 4]
 	}
 ];
